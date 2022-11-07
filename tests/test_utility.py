05f94b1f75a8281e324200da92c73a8ad8341951
Tch1b0 <Jpour3006@gmail.com>
2022-11-07 17:33:17 +0100

    tests: add `get_first_match` and `get_all_matches` utility tests

--- tests/test_utility.py
+++ tests/test_utility.py
@@ -1,5 +1,6 @@
 import setup_test
 from src.utility.general import is_email
+from src.utility.generics import get_first_match, get_all_matches
 
 
 def test_email_verification():
@@ -8,3 +9,19 @@ def test_email_verification():
     assert not is_email("@server.de")
     assert not is_email("me@server")
     assert is_email("me@server.de")
+
+
+def test_get_first_match():
+    assert get_first_match(lambda x: x[0] == "T", [
+                           "Test", "best", "Nest"]) == "Test"
+    assert get_first_match(lambda x: x.endswith("est") or x[0] == "T", [
+                           "Nest", "Test", "best"]) == "Nest"
+
+
+def test_get_all_matches():
+    pool = ["Hello", "test", "Something"]
+
+    assert get_all_matches(lambda x: x[0].isupper(), pool) == [
+        "Hello", "Something"]
+    assert get_all_matches(lambda x: x[0].islower(), pool) == [
+        "test"]
